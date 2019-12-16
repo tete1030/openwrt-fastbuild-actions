d2b75eec538b01d284654e9ee72813dab71d7ed1
Texot <tete1030@gmail.com>
2019-12-16 21:37:30 +0800

    Fix a typo in scripts/initenv.sh

--- scripts/initenv.sh
+++ scripts/initenv.sh
@@ -3,7 +3,7 @@
 set -eo pipefail
 
 echo "Installing building components ..."
-sudo -E apt-get -qq update \
+sudo -E apt-get -qq update
 sudo -E apt-get -qq install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler
 sudo -E apt-get -qq install tar wget curl nginx
 sudo -E apt-get -qq autoremove --purge
