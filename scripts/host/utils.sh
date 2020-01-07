2bdcca30a34acb67e95a594e9555cca8b691b336
Texot <tete1030@gmail.com>
2020-01-07 22:08:52 +0800

    Consider a bug in Github Actions Runner

--- scripts/host/utils.sh
+++ scripts/host/utils.sh
@@ -12,11 +12,19 @@ _check_missing_vars() {
 
 _set_env() {
     for var_name in $@ ; do
+        # unescape %0A and %0D, source: https://github.community/t5/GitHub-Actions/set-output-Truncates-Multiline-Strings/td-p/37870
+        # but no unescape of %25 ??? https://github.com/actions/runner/blob/6c70d53eea/src/Runner.Common/ActionCommand.cs#L19
         var_value="${!var_name}"
-        var_value="${var_value//'%'/'%25'}"
-        var_value="${var_value//$'\n'/'%0A'}"
-        var_value="${var_value//$'\r'/'%0D'}"
-        echo "::set-env name=${var_name}::${var_value}"
+        if [[ "${var_value}" == *"%0A"* || "${var_value}" == *"%0D"* ]]; then
+            echo "::error::Sadly, Github Action Runner unescapes '%0A' and '%0D' but does not unescape '%25'. You cannot contain '%0A' or '%0D' in the string" >&2
+            exit 1
+        fi
+        # var_value="${var_value//%/%25}"
+        var_value="${var_value//$'\n'/%0A}"
+        var_value="${var_value//$'\r'/%0D}"
+        echo -e "\n"
+        echo -n "::set-env name=${var_name}::${var_value}"
+        echo -e "\n"
     done
 }
 
