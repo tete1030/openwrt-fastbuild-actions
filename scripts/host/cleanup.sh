dc94a08537dfd1716df8ebc844c53cc3e6f3695f
Texot <tete1030@gmail.com>
2020-02-25 23:06:55 +0800

    Free up more space

--- scripts/host/cleanup.sh
+++ scripts/host/cleanup.sh
@@ -7,8 +7,8 @@ set +eo pipefail
 export DEBIAN_FRONTEND=noninteractive
 
 echo "Deleting files, please wait ..."
-sudo rm -rf /usr/share/dotnet /etc/apt/sources.list.d/*
+sudo rm -rf /usr/share/dotnet /etc/apt/sources.list.d/* /var/cache/apt/archives /usr/local/share/boost /usr/local/go* /usr/local/lib/android /opt/ghc
 sudo swapoff /swapfile
 sudo rm -f /swapfile
 docker rmi "$(docker images -q)"
-sudo -E apt-get -q purge azure-cli ghc* zulu* hhvm llvm* firefox google* dotnet* powershell openjdk* mysql*
+sudo -E apt-get -q purge azure-cli zulu* hhvm llvm* firefox google* dotnet* powershell openjdk* mysql*
