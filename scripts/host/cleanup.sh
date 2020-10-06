a145102334fa380e44e9dfd961a54e038fdd5e13
Texot <tete1030@gmail.com>
2020-10-06 15:24:41 +0800

    Upgrade workflows

--- scripts/host/cleanup.sh
+++ scripts/host/cleanup.sh
@@ -7,9 +7,10 @@ set +eo pipefail
 export DEBIAN_FRONTEND=noninteractive
 
 echo "Deleting files, please wait ..."
-sudo rm -rf /usr/share/dotnet /etc/apt/sources.list.d/* /var/cache/apt/archives /usr/local/share/boost /usr/local/go* /usr/local/lib/android /opt/ghc
-sudo swapoff /swapfile
+sudo rm -rf /opt/hostedtoolcache /usr/share/dotnet /etc/apt/sources.list.d/* /var/cache/apt/archives /usr/local/share/boost /usr/local/go* /usr/local/lib/android /opt/ghc
+sudo swapoff /swapfile 2>/dev/null
 sudo rm -f /swapfile
-docker rmi "$(docker images -q)"
+docker rm $(docker ps -aq)
+docker rmi $(docker images -q)
 sudo -E apt-get -q purge azure-cli zulu* hhvm llvm* firefox google* dotnet* powershell openjdk* mysql*
 exit 0
