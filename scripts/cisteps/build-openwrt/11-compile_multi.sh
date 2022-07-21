3782a94c71e8716c30d7916e276d42492b7f3fd5
Texot <tete1030@gmail.com>
2022-07-21 13:41:49 +0800

    Support detect failure package

--- scripts/cisteps/build-openwrt/11-compile_multi.sh
+++ scripts/cisteps/build-openwrt/11-compile_multi.sh
@@ -5,6 +5,10 @@ set -eo pipefail
 # shellcheck disable=SC1090
 source "${HOST_WORK_DIR}/scripts/host/docker.sh"
 
+if [ -f "/tmp/failed_packages.txt" ]; then
+    rm -f /tmp/failed_packages.txt
+fi
+
 echo "::set-output name=started::1"
 docker_exec -e MODE=m "${BUILDER_CONTAINER_ID}" "${BUILDER_WORK_DIR}/scripts/compile.sh"
 echo "::set-output name=status::success"
--- scripts/compile.sh
+++ scripts/compile.sh
@@ -19,10 +19,14 @@ if [ "x${TEST}" = "x1" ]; then
   exit 0
 fi
 
+save_output() {
+  stdbuf -oL -eL tee /tmp/compile_output.txt
+}
+
 trickle () {
-    gawk 'BEGIN { t = systime() }
-               { printf("[%5d] ", systime() - t) ; print $0 ; system("") }
-               { t = systime() }'
+  gawk 'BEGIN { t = systime() }
+              { printf("[%5d] ", systime() - t) ; print $0 ; system("") }
+              { t = systime() }'
 }
 
 compile() {
@@ -31,10 +35,10 @@ compile() {
     if [ "x${MODE}" = "xm" ]; then
       local nthread=$(($(nproc) + 1)) 
       echo "${nthread} thread compile: $*"
-      make -j${nthread} "$@" | trickle
+      make -j${nthread} "$@" | save_output | trickle
     elif [ "x${MODE}" = "xs" ]; then
       echo "Fallback to single thread compile: $*"
-      make -j1 V=s "$@" | trickle
+      make -j1 V=s "$@" | save_output | trickle
     else
       echo "No MODE specified" >&2
       exit 1
@@ -48,9 +52,46 @@ if [ -f "${BUILDER_PROFILE_DIR}/pre_compile.sh" ]; then
 fi
 
 echo "Compiling..."
-if [ "x${OPT_PACKAGE_ONLY}" != "x1" ]; then
-  compile
+set +eo pipefail
+last_status=0
+
+prev_failure_package=
+if [ -f "/tmp/failed_packages.txt" ]; then
+  prev_failure_package="$(cat /tmp/failed_packages.txt | head -n 1)"
+  rm -f /tmp/failed_packages.txt
+fi
+
+if [ -e "${prev_failure_package}" ]; then
+  if [ "x${OPT_PACKAGE_ONLY}" != "x1" ]; then
+    compile
+    last_status=$?
+  else
+    compile "package/compile"
+    last_status=$?
+    if [ $last_status -eq 0 ]; then
+      compile "package/index"
+      last_status=$?
+    fi
+  fi
 else
-  compile "package/compile"
-  compile "package/index"
+  echo "Compiling only previously failed package: ${prev_failure_package}"
+  compile "${prev_failure_package}"
+  last_status=$?
+fi
+
+if [ $last_status -ne 0 ]; then
+  echo "::error::Compile has failed" >&2
+  echo -n "::error::" >&2
+  grep -i "error:" /tmp/compile_output.txt >&2
+
+  # ERROR: package/feeds/packages/qemu failed to build.
+  re='ERROR:\s+([^\s]+)\s+failed to build'
+  if grep -P "$re" -o /tmp/compile_output.txt >/dev/null ; then
+    echo "::error::Failed packages:" >&2
+    grep -P "$re" -o /tmp/compile_output.txt | tr -s ' ' |  cut -d ' ' -f2 | tee /tmp/failed_packages.txt >&2
+  else
+    if [ -f "/tmp/failed_packages.txt" ]; then
+      rm -f /tmp/failed_packages.txt
+    fi
+  fi
 fi
